e76d70a4feb53ce52783fee0afca56f2f567e49b
Clemente Morales Fernández <morales.fernandez.clemente@gmail.com>
2015-07-22 17:55:54 -0500

    PopularMovies feature Add fragment to list movies.

---
+++ app/src/main/java/lania/edu/mx/popularmovies/fragments/MovieListFragment.java
@@ -0,0 +1,21 @@
+package lania.edu.mx.popularmovies.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import lania.edu.mx.popularmovies.R;
+
+/**
+ * Created by clemente on 7/22/15.
+ */
+public class MovieListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_movie_list, container, false);
+    }
+}
