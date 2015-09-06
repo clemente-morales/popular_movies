ebcba69657d4d4abdcbbdff2dc6a3936976ea294
Clemente Morales Fernández <morales.fernandez.clemente@gmail.com>
2015-09-06 14:52:30 -0500

    PopularMovies feature Add Toolbar instead of ActionBar.

--- app/src/main/java/lania/edu/mx/popularmovies/activities/MainActivity.java
+++ app/src/main/java/lania/edu/mx/popularmovies/activities/MainActivity.java
@@ -2,7 +2,8 @@ package lania.edu.mx.popularmovies.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -15,7 +16,7 @@ import lania.edu.mx.popularmovies.events.otto.SortOrderChangedEvent;
 import lania.edu.mx.popularmovies.fragments.MovieDetailActivityFragment;
 import lania.edu.mx.popularmovies.models.Movie;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     public static final String DETAIL_FRAGMENT_TAG = "MovielDetailFragmentTag";
     private boolean twoPane;
@@ -24,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (findViewById(R.id.movie_detail_container) != null) {
             twoPane = true;
--- app/src/main/java/lania/edu/mx/popularmovies/activities/MovieDetailActivity.java
+++ app/src/main/java/lania/edu/mx/popularmovies/activities/MovieDetailActivity.java
@@ -2,6 +2,7 @@ package lania.edu.mx.popularmovies.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
 import lania.edu.mx.popularmovies.R;
 
@@ -12,6 +13,9 @@ public class MovieDetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
